3f4e8c2be9b5a7a50a3aa2b5b4d1224fa428f752
hifishch <56207177+hifishch@users.noreply.github.com>
2019-10-05 23:44:41 +0200

    Initial Upload / Version 1.0.0

---
+++ MARS_BackupCheck.ps1
@@ -0,0 +1,37 @@
+﻿#Powershell MARS Backup Check for Solarwinds RMM
+#Author: Andreas Walker andreas.walker@walkerit.ch
+#Licence: GNU General Public License v3.0
+#Version: 1.0.0 / 05.10.2019
+
+#Check if MARS is installed
+if (!(Test-Path 'C:\Program Files\Microsoft Azure Recovery Services Agent\bin\Modules\MSOnlineBackup'))
+    {
+    Write-Host "MARS is not installed. Quitting."
+    Exit 1001
+    }
+
+#Import PS-Module
+Import-Module -Name 'C:\Program Files\Microsoft Azure Recovery Services Agent\bin\Modules\MSOnlineBackup'
+
+#Get last Backup
+$LastRestorePoint = Get-OBAllRecoveryPoints | Select-Object -first 1
+
+#Set Time-Variables
+$RefTime = (Get-Date).AddHours(-24)
+
+#Generating Output
+if ($LastRestorePoint.BackupTime -ge $RefTime)
+    {
+    Write-Host "OK"
+    Write-Host "The last backup has been preformed at"$LastRestorePoint.BackupTime
+    Exit 0
+    }
+else
+    {
+    Write-Host "WARNING: Last backup older than 24 hours"
+    Write-Host "The last backup has been preformed at"$LastRestorePoint.BackupTime
+    Exit 1001
+    }
+
+Write-Host "The Script came to an unexpected end."
+Exit 1001
